d6a2f71b4a91c4aeb4366a8e6004587f052f8c5c
Martin Giger <martin@humanoids.be>
2018-01-15 22:47:17 +0100

    test: add lint

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const PORT = process.env.PORT || 5000;
-const Controller = require("./lib");
-const sendCalendar = require("./lib/send-calendar");
+"use strict";
+
+const express = require("express"),
+    Controller = require("./lib"),
+    sendCalendar = require("./lib/send-calendar"),
+    DEFAULT_PORT = 5000,
+    PORT = process.env.PORT || DEFAULT_PORT;
 
 express()
     .get('/channel/:channelId', (req, res) => {
-        sendCalendar(Controller.GetChannelCalendar(req.params.channelId), res);
+        sendCalendar(Controller.getChannelCalendar(req.params.channelId), res);
     })
     .get('/following/:userId', (req, res) => {
-        sendCalendar(Controller.GetFollowsCalendar(req.params.userId), res);
+        sendCalendar(Controller.getFollowsCalendar(req.params.userId), res);
     })
     .listen(PORT);

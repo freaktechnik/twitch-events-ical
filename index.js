196be2c7cc7b776b12456661ead4d9f988edfb97
Martin Giger <martin@humanoids.be>
2018-01-16 12:44:15 +0100

    feat: use rendering

--- index.js
+++ index.js
@@ -4,10 +4,38 @@ const express = require("express"),
     bodyParser = require("body-parser"),
     Controller = require("./lib"),
     sendCalendar = require("./lib/send-calendar"),
-    SEE_ALSO = 303,
     DEFAULT_PORT = 5000,
     PORT = process.env.PORT || DEFAULT_PORT,
-    app = express();
+    app = express(),
+    render = (link = '') => {
+        let more = '';
+        if(link) {
+            more = `<p>Calendar URL: <a href="https://${process.env.HEROKU_APP_NAME}.herokuapp.com${link}">https://${process.env.HEROKU_APP_NAME}.herokuapp.com${link}</a></p>`;
+        }
+        return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Twitch Events iCal Generator</title>
+</head>
+<body>
+    <main>
+        <h1>Get iCal for</h1>
+        <form method="post" action="">
+            <p><input type="text" name="username" placeholder="Username"></p>
+            <p>
+                <button type="submit" name="submit" value="channel">Channel Events</button>
+                <button type="submit" name="submit" value="following">Following Channels Events</button>
+            </p>
+        </form>
+        ${more}
+    </main>
+    <footer>
+        <p><a href="https://github.com/freaktechnik/twitch-events-ical">Source Code</a></p>
+    </footer>
+</body>
+</html>`;
+    };
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,56 +47,13 @@ app.get('/following/:userId', (req, res) => {
 });
 app.route('/')
     .get((req, res) => {
-        res.send(`<!DOCTPYE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Twitch Events iCal Generator</title>
-</head>
-<body>
-    <main>
-        <h1>Get iCal for</h1>
-        <form method="post" action="">
-            <p><input type="text" name="username" placeholder="Username"></p>
-            <p>
-                <button type="submit" name="submit" value="channel">Channel Events</button>
-                <button type="submit" name="submit" value="following">Following Channels Events</button>
-            </p>
-        </form>
-    </main>
-    <footer>
-        <p><a href="https://github.com/freaktechnik/twitch-events-ical">Source Code</a></p>
-    </footer>
-</body>
-</html>`);
+        res.send(render());
     })
     .post((req, res) => {
         const type = req.body.submit,
-            username = req.body.username;
+            { username } = req.body;
         Controller.getUserId(username).then((id) => {
-            res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Twitch Events iCal Generator</title>
-</head>
-<body>
-    <main>
-        <h1>Get iCal for</h1>
-        <form method="post" action="">
-            <p><input type="text" name="username" placeholder="Username"></p>
-            <p>
-                <button type="submit" name="submit" value="channel">Channel Events</button>
-                <button type="submit" name="submit" value="following">Following Channels Events</button>
-            </p>
-        </form>
-        <p>Calendar URL: <a href="https://twitch-events-ical.herokuapp.com${link}">https://twitch-events-ical.herokuapp.com${link}</a></p>
-    </main>
-    <footer>
-        <p><a href="https://github.com/freaktechnik/twitch-events-ical">Source Code</a></p>
-    </footer>
-</body>
-</html>`);
+            res.send(render(`${type}/${id}`));
         })
             .catch(console.error);
     });
